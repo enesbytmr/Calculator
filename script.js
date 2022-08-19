3ee474330540ff774a9885b6da4c0b8b1356e526
Enes Baytemir <enesbytmr@gmail.com>
2022-08-19 14:43:29 +0300

    html css js basis doned

---
+++ script.js
@@ -0,0 +1,41 @@
+const add = (num1, num2 ) => {
+    return num1+ num2;
+}
+
+const subtract =(num1,num2)=>{
+return num1- num2;
+}
+
+const multiply = (num1,num2) =>{
+    return num1*num2
+}
+
+const divide = (num1,num2) =>{
+    return num1/num2
+}
+
+const operate=(num1,num2,operator)=>{
+
+return 1;
+}
+
+
+//buttons
+
+const display = document.getElementById("display")
+const allButtons = document.querySelectorAll('.button')
+
+const clear = document.querySelector('.clearBtn');
+const decimal = document.querySelector('.decimal')
+const equal = document.querySelector('.equal');
+
+const numbers = document.querySelectorAll('.numbers')
+const operators = document.querySelectorAll('.operators')
+
+const previous = document.querySelector('.previous');
+const current = document.querySelector('.current')
+
+
+
+
+
