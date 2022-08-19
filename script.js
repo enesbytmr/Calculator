ddc05b8d87555a9dd0528c96b6df7d51e3b802e1
Enes Baytemir <enesbytmr@gmail.com>
2022-08-19 18:53:24 +0300

    finished

--- script.js
+++ script.js
@@ -1,41 +1,111 @@
-const add = (num1, num2 ) => {
-    return num1+ num2;
-}
+let currentValue = "";
+let operator = "";
+let previousValue = "";
+let cNum =""
+const display = document.getElementById("display");
+const allButtons = document.querySelectorAll(".button");
 
-const subtract =(num1,num2)=>{
-return num1- num2;
-}
+const clear = document.querySelector("#clearBtn");
+const decimal = document.querySelector(".decimal");
+const equal = document.querySelector(".equal");
+const deleteBtn = document.querySelector('#delete')
 
-const multiply = (num1,num2) =>{
-    return num1*num2
-}
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
 
-const divide = (num1,num2) =>{
-    return num1/num2
-}
-
-const operate=(num1,num2,operator)=>{
-
-return 1;
-}
-
+let previousScreen = document.querySelector(".previous");
+let currentScreen = document.querySelector(".current");
 
 //buttons
 
-const display = document.getElementById("display")
-const allButtons = document.querySelectorAll('.button')
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    handleNumber(e.target.textContent);
+    currentScreen.textContent = currentValue;
+  });
+});
 
-const clear = document.querySelector('.clearBtn');
-const decimal = document.querySelector('.decimal')
-const equal = document.querySelector('.equal');
+operators.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    handleOperator(e.target.textContent);
+  });
+});
 
-const numbers = document.querySelectorAll('.numbers')
-const operators = document.querySelectorAll('.operators')
+clear.addEventListener('click',()=>{
+    currentValue = ''
+    previousValue = ''
+    operator = ''
+    currentScreen.innerText=""
+    previousScreen.innerText= ""
+})
 
-const previous = document.querySelector('.previous');
-const current = document.querySelector('.current')
+deleteBtn.addEventListener('click',()=>{
+    currentValue = currentValue.substring(0, currentValue.length - 1);
+    currentScreen.innerHTML =currentValue
 
 
+})
 
+let handleNumber = (num) =>{
+    if(currentValue.length <= 15){
+    currentValue += num;}
+    else{
+        currentScreen.innerText = "error"
+    }
+}
 
+let handleOperator =(op) => {
+    operator = op;
+    previousScreen.textContent = currentValue + " "+ operator
+    previousValue = currentValue;
+    currentValue = ""
+    currentScreen.innerText= ""
+    
+}
 
+equal.addEventListener('click' ,()=>{ 
+    currentValue = Number(currentValue);
+    previousValue =Number(previousValue);
+    operate()})
+
+const add = (num1, num2) => {
+    
+    cNum= num1 + num2
+    previousScreen.innerText="";
+    currentScreen.innerText=cNum;
+};
+
+const subtract = (num1, num2) => {
+    cNum = num2 - num1 
+    previousScreen.innerText="";
+    currentScreen.innerText=cNum;
+};
+
+const multiply = (num1, num2) => {
+    cNum = num1 * num2
+    previousScreen.innerText="";
+    currentScreen.innerText=cNum;
+};
+
+const divide = (num1, num2) => {
+  cNum = num1 / num2
+  previousScreen.innerText="";
+  currentScreen.innerText=cNum;
+
+};
+
+const operate = () => {
+    
+    if(operator == '+'){
+        add(currentValue,previousValue)
+    }else if(operator == '-'){
+        subtract(currentValue,previousValue);
+    }else if(operator=='*'){
+        multiply(currentValue,previousValue);
+    }else if(operator== '/'){
+        divide(currentValue,previousValue);
+    }
+    currentValue = ""
+    previousValue = ""
+    
+};
